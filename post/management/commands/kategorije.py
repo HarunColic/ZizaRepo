bdaedb06d302765a32f54a1c08776922c7b0ded1
Harun <haruncolic@hotmail.com>
2018-10-05 19:27:42 +0200

    nove kategorije

--- post/management/commands/kategorije.py
+++ post/management/commands/kategorije.py
@@ -10,24 +10,23 @@ from post.models import Category
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-
         categories = [
             'Poljoprivredne',
             'Šumarske',
             'Ribolovne',
             'Rudarske',
             'Prerađivačke',
-            'Energetske', #ENERGETSKE
-            'Vodene i ekološke',#VODENE I EKOLOSKE
+            'Energetske',  # ENERGETSKE
+            'Vodene i ekološke',  # VODENE I EKOLOSKE
             'Građevinske',
-            'Trgovačke' #TRGOVINA
-            'Automehaničarske', #SERVIS VOZILA
+            'Trgovačke'  # TRGOVINA
+            'Automehaničarske',  # SERVIS VOZILA
             'Prijevozne i skladišne',
-            'Ugostiteljske', # HORECA
-            'IT', #IT
-            'Finansijske', #FINANSIJSKE DJELATNOSTI
-            'Osiguravajuće',  #OSIGURANJA
-            'Poslovanje nekretninama', #NEKRETNINE
+            'Ugostiteljske',  # HORECA
+            'IT',  # IT
+            'Finansijske',  # FINANSIJSKE DJELATNOSTI
+            'Osiguravajuće',  # OSIGURANJA
+            'Poslovanje nekretninama',  # NEKRETNINE
             'Stručne, '
             'Znanstvene'
             'Tehničke',
@@ -39,7 +38,7 @@ class Command(BaseCommand):
             'Umjetničke',
             'Zabavne i rekreacijske',
             'Zdravstvene',
-            ]
+        ]
         categories2 = [
 
             'Administrativne i slične usluge',
@@ -55,11 +54,50 @@ class Command(BaseCommand):
             'Grafička industrija',
             'Grafički dizajn',
         ]
-
-        for c in categories:
-            cat = Category(name=c, type=0)
-            cat.save()
-
-        for c in categories2:
+        categories3 = [
+            'Građevinarstvo',
+            'Informatika - Hardware',
+            'Informatika - Software',
+            'IT',
+            'Komercijala - Prodaja',
+            'Konsalting',
+            'jepota i zdravlje',
+            'Mali oglasi',
+            'Marketing - PR',
+            'Mediji',
+            'Menadžment i upravljanje',
+            'Nauka - Istraživački rad',
+            'Nekretnine',
+            'Obrazovanje',
+            'Oglasi ZZZRS',
+            'Osiguranje',
+            'Ostalo',
+            'Policija - Zaštitarske usluge',
+            'Poljoprivreda - Ribarstvo - Šumarstvo',
+            'Pravo',
+            'Prehrambena Industrija',
+            'Proizvodnja',
+            'Računovodstvo - Revizija',
+            'Rudarstvo',
+            'Saobraćaj i komunikacije',
+            'Socijalne usluge - Neprofitne organizacije',
+            'Telekomunikacije',
+            'Transport - Skladištenje i logistika',
+            'Turizam',
+            'Ugostiteljstvo',
+            'Umjetnost i dizajn',
+            'Veterina',
+            'Zabava',
+            'Zanatske usluge',
+            'Zdravstvo',
+        ]
+        # for c in categories:
+        #     cat = Category(name=c, type=0)
+        #     cat.save()
+        #
+        # for c in categories2:
+        #     cat = Category(name=c, type=1)
+        #     cat.save()
+        for c in categories3:
             cat = Category(name=c, type=1)
             cat.save()
