cc9a38ebda8569a7a397a02588da18b4eed24491
HarunColic <haruncolic@hotmail.com>
2018-10-07 13:56:46 -0400

    commit

--- static/skripta.js
+++ static/skripta.js
@@ -156,6 +156,8 @@ var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+
 btn.onclick = function() { modal.style.display = "block";  }
 span.onclick = function() {
     modal.style.display = "none";
@@ -171,6 +173,14 @@ btn2.onclick = function() { modal2.style.display = "block"; }
 span2.onclick = function() { modal2.style.display = "none"; }
 window.onclick = function(event) {
     if (event.target == modal2) { modal2.style.display = "none"; }
+}
+
+var modal3 = document.getElementById('myModal3');
+var btn3 = document.getElementById("myBtn3");
+btn3.onclick = function() { modal3.style.display = "block"; }
+span3.onclick = function() { modal3.style.display = "none"; }
+window.onclick = function(event) {
+    if (event.target == modal3) { modal3.style.display = "none"; }
 }
 
 $('.single-kucica').mouseover(function(){
