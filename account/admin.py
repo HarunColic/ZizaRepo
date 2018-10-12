da89871381ef5e3f8a012cf3bac903f2127bef92
Harun <haruncolic@hotmail.com>
2018-10-12 23:10:10 +0200

    com

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from account import models as Amodels
+from post import models as Pmodels
 
 # Register your models here.
+
+admin.site.register(Amodels.UserProfile)
+admin.site.register(Amodels.Employee)
+admin.site.register(Amodels.Company)
+admin.site.register(Pmodels.Category)
+admin.site.register(Pmodels.Post)
+admin.site.register(Pmodels.PostCategories)
+admin.site.register(Pmodels.WorkersPosts)
+admin.site.register(Pmodels.UserCategories)
+admin.site.register(Pmodels.Tag)
+admin.site.register(Pmodels.FAQ)
+admin.site.register(Pmodels.PostTags)
