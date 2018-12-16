4d72d05e58e6726ea9a3b40b371bd00f90dcfc44
HarunColic <haruncolic@hotmail.com>
2018-12-16 18:15:47 +0100

    ASD

---
+++ post/management/commands/logotipMail.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand, CommandError
+from account.models import UserProfile, User
+from post.models import Post
+from datetime import datetime
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from post.models import Category
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        userPs = UserProfile.objects.filter(image=None)
+        mails = []
+
+        for up in userPs:
+
+            this = User.objects.get(UserProfile=up)
+            mails.append(this.email)
+
+        mail_subject = "Mailovi korisnika bez logotipa"
+        message = render_to_string('MailoviBezLogotipa.html', {
+
+            'mails': mails,
+
+        })
+
+        email = EmailMessage(
+            mail_subject, message, to=['haruncolic@hotmail.com']
+        )
+
+        email.send()
