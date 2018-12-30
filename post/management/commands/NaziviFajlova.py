71dd7b28a9be872e519ca964171c8faf8f642ee6
HarunColic <haruncolic@hotmail.com>
2018-12-30 21:48:29 +0100

    ASD

--- post/management/commands/NaziviFajlova.py
+++ post/management/commands/NaziviFajlova.py
@@ -30,5 +30,4 @@ class Command(BaseCommand):
             elementi = filename.split('.')
 
             if elementi[elementi.__len__()-1] == '.docx' or elementi[elementi.__len__()-1] == '.doc' or elementi[elementi.__len__()-1] == '.pdf':
-                os.rename(filename, filename.replace(" ", "-"))
-
+                print(filename)
