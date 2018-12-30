f72611fdf280e18a59c27f54875ea2d882c5e9d3
HarunColic <haruncolic@hotmail.com>
2018-12-30 21:35:10 +0100

    ASD

--- post/management/commands/NaziviFajlova.py
+++ post/management/commands/NaziviFajlova.py
@@ -6,22 +6,28 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from post.models import Category
 from django.template.defaultfilters import slugify
-
+import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        userPs = UserProfile.objects.all()
-        oglasi = Post.objects.all()
+      #  userPs = UserProfile.objects.all()
+       # oglasi = Post.objects.all()
 
-        for u in userPs:
+        #for u in userPs:
 
-            if u.cv:
-                u.cv.name = slugify(u.cv.name)
+         #   if u.cv:
+          #      u.cv.name = slugify(u.cv.name)
 
-        for o in oglasi:
+        #for o in oglasi:
 
-            if o.attachment:
-                o.attachment.name = slugify(o.attachment.name)
+         #   if o.attachment:
+          #      o.attachment.name = slugify(o.attachment.name)
 
+        for filename in os.listdir(os.path.abspath('Ziza/static/media')):
+
+            elementi = filename.split('.')
+
+            if elementi[elementi.__len__()-1] == '.docx' or elementi[elementi.__len__()-1] == '.doc' or elementi[elementi.__len__()-1] == '.pdf':
+                filename = slugify(filename)
