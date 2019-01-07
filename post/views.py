0367bde04934de686d0620a6f304c568f38d3a3d
HarunColic <haruncolic@hotmail.com>
2019-01-07 15:47:11 +0100

    ASD

--- post/views.py
+++ post/views.py
@@ -102,7 +102,7 @@ def createpost(request):
             if expiration is not '0':
                 post.expires_at = datetime.now()+timedelta(days=int(expiration))
             else:
-                post.expires_at = datetime.now()+timedelta(days=99999)
+                post.expires_at = datetime.now()+timedelta(years=999)
 
             post.attachment = myfile
             post.save()
@@ -460,7 +460,7 @@ def updatePost(request, id):
             if trajanje is not '0':
                 post.expires_at = datetime.now() + timedelta(days=int(trajanje))
             else:
-                post.expires_at = datetime.now() + timedelta(days=99999)
+                post.expires_at = datetime.now() + timedelta(years=999)
 
             post.save()
 
